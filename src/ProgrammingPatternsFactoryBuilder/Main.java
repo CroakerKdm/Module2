9bb516a937109e31d53ecef4bc05a886ba6cdbf3
Croaker <CroakerKdm@gmail.com>
2021-03-30 18:28:45 +0300

    Initial

---
+++ src/ProgrammingPatternsFactoryBuilder/Main.java
@@ -0,0 +1,13 @@
+package ProgrammingPatternsFactoryBuilder;
+
+/**
+ * @author Croaker
+ * @version 1.0.0
+ * @project Module2
+ * @class Main
+ * @since 29.03.2021 - 18.28
+ **/
+public class Main {
+
+
+}
